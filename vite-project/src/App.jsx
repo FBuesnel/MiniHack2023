795be045ecd26c935ae246d195348f2a41d8f719
JorYin <134739719+JorYin@users.noreply.github.com>
2023-10-07 14:57:07 -0400

    u

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,44 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 
-
 function App() {
+  const [count, setCount] = useState(0);
+  const [carbonData, setCarbonData] = useState(null);
 
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const API_KEY = 'RnqzyslpQBxuBk53fSyonQ';
+        const url = 'https://www.carboninterface.com/api/v1/auth';
 
-  const fetchData = async () => {
-    try {
-      const Authorization = 'RnqzyslpQBxuBk53fSyonQ'
-      const response = await axios.get('https://www.carboninterface.com/api/v1/estimates');
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+        const headers = {
+          Authorization: `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json',
+        };
 
-  fetchData();
-  
+
+        const requestData = {
+          type: 'electricity',
+          electricity_unit: 'mwh',
+          electricity_value: 42,
+          country: 'us',
+          state: 'fl',
+        };
+
+        const response = await axios.get(url, { params, headers });
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures this effect runs once after the initial render
+
+
   return (
     <>
       <div>
@@ -43,6 +61,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div className="carbon-data">
+          <h2>Carbon Data:</h2>
+          <pre>{JSON.stringify(carbonData, null, 2)}</pre>
+        </div>
     </>
   )
 }
