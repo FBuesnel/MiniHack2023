fefbe5050a74e35c1205ae41a90100d1c6271b17
FBuesnel <fynnbuesnel@gmail.com>
2023-10-07 11:36:19 -0400

    Template Addition
    Added a template from online and integrated it with flask.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/generic")
+def generic():
+    return render_template("generic.html")
+
+@app.route("/elements")
+def elements():
+    return render_template("elements.html")
